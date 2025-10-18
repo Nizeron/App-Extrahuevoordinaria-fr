865318801b0c0ee16331e8be075924a4d895ef24
Nizeron <nizeron26@gmail.com>
2025-10-18 19:40:36 -0300

    6

--- app/views.py
+++ app/views.py
@@ -11,24 +11,4 @@ def index(request):
 def juego(request):
     return render(request, 'app/juego.html')
 
-@login_required
-def guardar_progreso(request):
-    if request.method == 'POST':
-        botellas = int(request.POST.get('botellas', 0))
-        desbloqueables = request.POST.getlist('desbloqueables[]')
-        progreso, created = Progreso.objects.get_or_create(user=request.user)
-        progreso.botellas = botellas
-        progreso.desbloqueables = desbloqueables
-        progreso.save()
-        return JsonResponse({'status': 'ok'})
-    return JsonResponse({'status': 'error'})
-
-@login_required
-def obtener_progreso(request):
-    progreso, created = Progreso.objects.get_or_create(user=request.user)
-    return JsonResponse({
-        'botellas': progreso.botellas,
-        'desbloqueables': progreso.desbloqueables
-    })
-
 # Comentario prueba

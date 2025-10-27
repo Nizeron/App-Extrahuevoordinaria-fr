68050e5882187c69a52172273bd4614947d36d76
sarntruck <alejandro.ruiz.yoyo@gmail.com>
2025-10-27 16:16:32 -0300

    fr

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Progreso(models.Model):
+class Items(models.Model):
     nombre = models.CharField(max_length=20, null=True,blank=True)
     costo = models.IntegerField(default=0,null=True,blank=True)
     about = models.TextField(default=20)

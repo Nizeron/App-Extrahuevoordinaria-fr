19f8a17063f191541b746f8d1493591fe29b9000
sarntruck <alejandro.ruiz.yoyo@gmail.com>
2025-10-27 13:11:53 -0300

    branch1

--- app/models.py
+++ app/models.py
@@ -3,12 +3,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Progreso(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    botellas = models.IntegerField(default=0)
-    desbloqueables = models.JSONField(default=list)
+    nombre = models.CharField(max_length=20, null=True,blank=True)
+    costo = models.IntegerField(default=0,null=True,blank=True)
+    about = models.TextField(default=20)
+    incremento = models.IntegerField(default=0,null=True,blank=True)
 
-    def __str__(self):
-        return f"{self.user.username} - {self.botellas} botellas"
-# Create your models here.
-
-# Create your models here.
